5f8da5ae1b88622c7ab06467cb184647d4f4754c
lkportal <lksxim@gmail.com>
2022-10-31 16:03:33 -0300

    learn js

---
+++ script.js
@@ -0,0 +1,15 @@
+function numb(numero){
+   
+   let num1 = document.getElementById('resultado').innerHTML = numero
+   if(num1 != null){
+    const numerosADD = []
+   numerosADD.push(numero)
+   console.log(numerosADD)
+   }
+  
+   
+    
+   
+}
+
+  
